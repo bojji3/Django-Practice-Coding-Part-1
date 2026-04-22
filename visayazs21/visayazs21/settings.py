beb6375170c5e6c5e26604a082a395aa5b19f918
Kris Visaya <kvisaya@gbox.adnu.edu.ph>
2026-04-22 16:09:22 +0800

    data models and 404

--- visayazs21/visayazs21/settings.py
+++ visayazs21/visayazs21/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^n^$2*_5^79#*na-t#)wb68^a&w*u9y@#8e+n5fy9!=0+6*lk9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
